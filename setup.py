7b07faf807d8969775cc3778679402c79f6915b1
Andrew Shi <andrew@syntechcorporation.com>
2019-01-15 20:45:58 -0800

    Bump autoflake

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['autoflake==1.1'],
+    install_requires=['autoflake==1.3-munkyshi'],
+    dependency_links=[
+        'git+ssh://git@github.com/munkyshi/autoflake.git#egg=autoflake-1.3-munkyshi',
+    ]
 )
